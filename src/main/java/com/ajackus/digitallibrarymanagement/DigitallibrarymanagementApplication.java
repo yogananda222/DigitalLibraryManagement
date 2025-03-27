3c668ca817377a21240177c2e131a95e801e8fbc
yogananda222 <yoganandanaidu222@gmail.com>
2025-03-27 19:47:02 +0530

    Your commit message here

---
+++ src/main/java/com/ajackus/digitallibrarymanagement/DigitallibrarymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.ajackus.digitallibrarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitallibrarymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitallibrarymanagementApplication.class, args);
+	}
+
+}
